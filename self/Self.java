8ca31d7904ef7d3e102c34c729f5de065299ceaa
JordanInoa <83612765+JordanInoa@users.noreply.github.com>
2021-08-03 21:27:05 -0400

    Add files via upload

---
+++ self/Self.java
@@ -0,0 +1,7 @@
+public class Self {
+    public static void main(String[] args) {
+        System.out.println("My name is Jordan");
+        System.out.println("I am 29 years old");
+        System.out.println("I am from New York City");
+    }
+}
